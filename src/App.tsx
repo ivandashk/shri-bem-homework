04821feef3ee0f6d90b0fd681b6278a98739c023
Ivan Dashkevich <idashkevich@lement.pro>
2018-11-16 17:07:49 +0300

    Удален компонент Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
+import { cn } from '@bem-react/classname';
+import { RegistryConsumer } from '@bem-react/di';
 import * as React from 'react';
 import './App.scss';
-import Page from './Components/Page/Page';
+import Events from './Components/Events/Events';
+
+const cnApp = cn('App');
+const cnPage = cn('Page');
+const cnHeader = cn('Header');
+const cnFooter = cn('Footer');
 
 export const App: React.SFC = () => (
-  <Page />
+  <RegistryConsumer>
+        {registries => {
+            const registry = registries[cnApp()];
+            
+            const Header = registry.get(cnHeader());
+            const Footer = registry.get(cnFooter());
+
+            return(
+            <div className={ cnPage() }>
+                <Header />
+                <Events />
+                <Footer />
+            </div>
+            );
+        }}
+    </RegistryConsumer>
 );
+
+export default App;
